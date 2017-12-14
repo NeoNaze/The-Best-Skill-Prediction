f7bb5a2ae560b47a398efa9a0052f782ba57ddc4
Neo <33120875+NeoNaze@users.noreply.github.com>
2017-12-14 18:48:03 +0100

    Add files via upload

---
+++ config/abnormalities.js
@@ -0,0 +1,47 @@
+module.exports = {
+	/* Slayer */
+	23220: true, // Distant Blade & ICB (linked)
+
+	/* Brawler */
+	10153060: true, // Punch (linked)
+	10153001: true, // Punch > Counter 1 (linked)
+	10153002: true, // Punch > Counter 2 (linked)
+	10153003: true, // Punch > Counter 3 (linked)
+	10153004: true, // Punch > Counter 4 (linked)
+	10153005: true, // Counter (linked)
+	10153006: true, // Counter (linked)
+
+	/* Ninja */
+	// Combo Attack
+	10154000: true,
+	10154001: true,
+	10154002: true,
+	10154003: true,
+	10154004: true,
+	10154005: true,
+	10154006: true,
+	
+	/* Gunner */
+    10152000: true,	// PB forward
+    10152001: true, // PB forward
+	10152002: true, // PB backward
+    10152010: true, // Time Bomb, Arcane Barrage fire & Rolling Reload, responsible for client sending action 2 of many next skills.
+	10152011: true, // Blast, responsible for client sending action 2 of many next skills too.
+    10152012: true, // Rolling Reload
+    10152040: true, // Arcane Barrage flying
+    10152081: true, // Arcane Barrage detonate
+    10152050: true, // Burst fire shoot
+    10152053: true, // Burst fire windup
+    10152054: true, // Burst fire shoot
+	10152071: true, // Rolling reload
+    10152072: true, // Replenishment
+	10152080: true, // Rolling Reload
+    10152082: true, // Bombardment cast
+    10152083: true, // Scatter Shot
+    10152084: true, // Time bomb fire
+    10152085: true, // Mana Missiles fire
+    10152086: true, // Arc Bomb
+	10152087: true, // Rocket Jump
+    10152097: true, // Balder's Vengeance
+    10153093: true // Rocket Jump & Rolling Reload
+}
